f6adc56bf212d54b9b144ab8b7f8a15b6e9d6939
Haynes Brotherton <hbrotherton44@gmail.com>
2020-08-18 21:57:45 -0400

    added const's and questions array

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown"); 
+
 // array of questions for user
 const questions = [
-
+   {type: "input", message: "What is the title of your project?", name: "Title"},
+   {type: "input", message: "Please provide a description of your project:", name: "Description"},
+   {type: "input", message: "Please provide any installations needed for your project:", name: "Installations"},
+   {type: "input", message: "Please provide the usage for your project:", name: "Usage"},
+   {type: "input", message: "Please provide any licenses needed for your project:", name: "License"},
+   {type: "input", message: "Please provide any collaborators:", name: "Credits"},
+   {type: "input", message: "Please provide commands for tests:", name: "Test"},
+   {type: "input", message: "Please provide ways for contribution:", name: "Contributing"}
 ];
 
 // function to write README file
