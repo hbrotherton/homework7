756f2276640dfd10c36899bbfd7ed36a70cd356b
Haynes Brotherton <hbrotherton44@gmail.com>
2020-08-18 22:21:18 -0400

    added license and contributor const's

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown"); 
 
+// Choose a license for my application
+const apache = "Licensed under the [Apache License](https://spdx.org/licenses/Apache-2.0.html).";
+const gnu    = "Licensed under the [GNU GPLv3 License](https://spdx.org/licenses/GPL-3.0-or-later.html).";
+const mit    = "Licensed under the [MIT License](https://spdx.org/licenses/MIT.html).";
+const isc    = "Licensed under the [ISC License](https://spdx.org/licenses/ISC.html).";
+
+// contributors
+const yesContributors = "If you would like to contribute to this project, please follow the [Contributor Covenant Code of Conduct](https://www.contributor-covenant.org/version/2/0/code_of_conduct/) guidelines."
+const noContributors  = "This project is currently not accepting any contributions."
+
 // array of questions for user
 const questions = [
    {type: "input", message: "What is the title of your project?", name: "Title"},
